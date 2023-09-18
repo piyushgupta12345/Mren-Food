a0505e1d4c05036cddc88366e73e336fcf31c12c
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-09-19 00:15:44 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <div>
+      <div><Navbar/></div>
+      <div>body</div>
+      <div><Footer/></div>
+    </div>
+  )
+}
+
+export default Home
